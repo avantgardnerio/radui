5cae1099897806b7cc5c56ff0b06f45c222a469b
Brent Gardner <bgardner@squarelabs.net>
2024-04-30 11:55:23 -0600

    dynamic layout

--- src/bin/demo.rs
+++ src/bin/demo.rs
@@ -3,25 +3,34 @@ use glfw_window::GlfwWindow;
 use piston_window::{clear, PistonWindow, rectangle, WindowSettings};
 use piston_window::ellipse::circle;
 use yaserde::de::{from_str};
-use radui::generated::models::{Windows};
+use radui::generated::models::{WidgetChoice, Windows};
 
 fn main() {
     println!("hello world");
 
     let filename = "resources/layout.xml";
     let content = fs::read_to_string(filename).unwrap();
-    let buffers: Windows = from_str(&content).unwrap();
-    println!("buffers={buffers:#?}");
-    
+    let windows: Windows = from_str(&content).unwrap();
+
+    let win = windows.window.get(0).expect("Expected at least 1 window");
+
     let mut window: PistonWindow<GlfwWindow> =
-        WindowSettings::new("title", [640, 480])
+        WindowSettings::new(win.title.as_str(), [win.width, win.height])
             .build().unwrap();
     while let Some(e) = window.next() {
         window.draw_2d(&e, |c, g, _| {
             clear([0.5, 0.5, 0.5, 1.0], g);
-            rectangle([1.0, 0.0, 0.0, 1.0], // red
-                      [0.0, 0.0, 100.0, 100.0], // rectangle
-                      c.transform, g);
+            match &win.child.widget_choice {
+                WidgetChoice::Box(b) => {
+                    rectangle([1.0, 0.0, 0.0, 1.0], // red
+                              [b.x, b.y, b.width, b.height],
+                              c.transform, g);
+                }
+                WidgetChoice::Circle(c) => {
+                    circle(c.x, c.y, c.radius);
+                }
+                WidgetChoice::__Unknown__(_) => {}
+            }
             circle(50.0, 50.0, 100.0);
         });
     }
