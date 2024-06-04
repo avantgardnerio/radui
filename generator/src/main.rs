9b8c83d5633cd6ab11f546d041d73e805699b7de
Brent Gardner <bgardner@squarelabs.net>
2024-06-04 07:47:28 -0600

    finding setters

--- generator/src/main.rs
+++ generator/src/main.rs
@@ -1,11 +1,13 @@
 use std::{env, fs};
 use as3_parser::compilation_unit::CompilationUnit;
+use as3_parser::ns::FunctionName;
 use as3_parser::parser::ParserFacade;
+use as3_parser::tree::Directive;
 use glob::glob;
 
 fn main() {
     let home = dirs::home_dir().unwrap();
-    let home = home.join("workspace/flex-sdk/frameworks/projects/framework/src/mx/core/*.as");
+    let home = home.join("workspace/flex-sdk/frameworks/projects/mx/src/mx/**/*.as");
     let home = home.to_str().unwrap();
     println!("home={home:?}");
 
@@ -16,5 +18,35 @@ fn main() {
         let source_path = source_path.to_str().unwrap().to_string();
         let compilation_unit = CompilationUnit::new(Some(source_path), source_content);
         let program = ParserFacade(&compilation_unit, Default::default()).parse_program();
+
+        for package in program.packages.iter() {
+            for directive in package.block.directives.iter() {
+                match directive.as_ref() {
+                    Directive::ClassDefinition(defn) => {
+                        for directive in defn.block.directives.iter() {
+                            match directive.as_ref() {
+                                Directive::FunctionDefinition(func) => {
+                                    match &func.name {
+                                        FunctionName::Identifier(_) => {}
+                                        FunctionName::Getter(_) => {}
+                                        FunctionName::Setter((name, loc)) => {
+                                            println!("set {name}");
+                                        }
+                                        FunctionName::Constructor(_) => {}
+                                    }
+                                }
+                                _ => {}
+                            }
+                            // if print {
+                            //     println!("{directive:#?}\n");
+                            // }
+                        }
+                    },
+
+                    _ => {},
+                }
+            }
+        }
+
     }
 }
