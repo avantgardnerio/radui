c6ee0811f6b03a0bebbcf8c619b2dc0d6fa11ced
Brent Gardner <bgardner@squarelabs.net>
2024-06-04 08:45:18 -0600

    class hierarchy

--- generator/src/main.rs
+++ generator/src/main.rs
@@ -1,16 +1,18 @@
 use std::{env, fs};
+use std::collections::{HashMap, HashSet};
 use as3_parser::compilation_unit::CompilationUnit;
-use as3_parser::ns::FunctionName;
+use as3_parser::ns::{Expression, FunctionName, QualifiedIdentifierIdentifier};
 use as3_parser::parser::ParserFacade;
 use as3_parser::tree::Directive;
 use glob::glob;
 
 fn main() {
     let home = dirs::home_dir().unwrap();
-    let home = home.join("workspace/flex-sdk/frameworks/projects/mx/src/mx/**/*.as");
+    let home = home.join("workspace/flex-sdk/frameworks/projects/**/*.as");
     let home = home.to_str().unwrap();
     println!("home={home:?}");
 
+    let mut extends = HashMap::<String, Vec<String>>::new();
     for e in glob(home).expect("Failed to read glob pattern") {
         let source_path = e.unwrap();
         println!("{}", source_path.display());
@@ -23,6 +25,18 @@ fn main() {
             for directive in package.block.directives.iter() {
                 match directive.as_ref() {
                     Directive::ClassDefinition(defn) => {
+                        let class_name = defn.name.0.to_string();
+                        println!("class={}", defn.name.0);
+                        if let Some(ext) = &defn.extends_clause {
+                            if let Expression::QualifiedIdentifier(id) = ext.as_ref() {
+                                if let QualifiedIdentifierIdentifier::Id((name, _)) = &id.id {
+                                    extends.entry(name.clone())
+                                        .and_modify(|hs| hs.push(class_name.clone()))
+                                        .or_insert_with(|| vec![class_name.clone()]);
+                                    println!("extends {name}");
+                                }
+                            }
+                        }
                         for directive in defn.block.directives.iter() {
                             match directive.as_ref() {
                                 Directive::FunctionDefinition(func) => {
@@ -30,7 +44,7 @@ fn main() {
                                         FunctionName::Identifier(_) => {}
                                         FunctionName::Getter(_) => {}
                                         FunctionName::Setter((name, loc)) => {
-                                            println!("set {name}");
+                                            // println!("set {name}");
                                         }
                                         FunctionName::Constructor(_) => {}
                                     }
