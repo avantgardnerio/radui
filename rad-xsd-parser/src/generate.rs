b88110d43b0e9ea02c2374cdb33e209ca45b4c70
Brent Gardner <bgardner@squarelabs.net>
2024-06-09 12:34:09 -0600

    generating plausible rust

--- rad-xsd-parser/src/generate.rs
+++ rad-xsd-parser/src/generate.rs
@@ -1,5 +1,6 @@
+use convert_case::{Case, Casing};
 use crate::logical::LogicalSchema;
-use crate::models::{AttribGroupEl, ComplexContentEl, ExtensionEl};
+use crate::models::{ComplexContentEl, ExtensionEl};
 
 pub fn generate(schema: LogicalSchema) -> String {
     schema
@@ -13,7 +14,7 @@ pub fn generate(schema: LogicalSchema) -> String {
                 return None;
             };
             let typ = typ.strip_prefix("mx:").unwrap().to_string();
-            let typ = schema.types.get(&typ).expect(format!("Can't find complexType: {typ}").as_str());
+            let mut typ = schema.types.get(&typ).expect(format!("Can't find complexType: {typ}").as_str());
             let mut attrs = vec![];
             let Some(content) = &typ.complex_content else {
                 return None;
@@ -21,39 +22,43 @@ pub fn generate(schema: LogicalSchema) -> String {
             let Some(content) = &content.content else {
                 return None;
             };
-            let ComplexContentEl::Extension(extension) = content else {
+            let ComplexContentEl::Extension(mut extension) = content.clone() else {
                 return None;
             };
             let Some(extensions) = &extension.extensions else {
                 return None;
             };
             for ext in extensions {
-                let ExtensionEl::AttributeGroup(grp) = ext else {
+                let ExtensionEl::Attribute(attr) = ext else {
                     continue;
                 };
-                let Some(reference) = &grp.reference else {
-                    continue;
+                let typ = match attr.typ.as_str() {
+                    "string" => "String",
+                    _ => panic!("Unknown type: {}", attr.typ),
                 };
-                let grp = reference.strip_prefix("mx:").unwrap().to_string();
-                let grp =
-                    schema.attribute_groups.get(&grp).expect(format!("Can't find attribute_group: {grp}").as_str());
-                let Some(attributes) = &grp.attributes else {
-                    continue;
-                };
-                for attr in attributes {
-                    let AttribGroupEl::Attribute(attr) = attr else {
-                        continue;
-                    };
-                    let typ = match attr.typ.as_str() {
-                        "string" => "String",
-                        _ => panic!("Unknown type: {}", attr.typ),
-                    };
-                    let str = format!("pub {}: {typ},", attr.name);
-                    attrs.push(str);
-                }
+                let str = format!("#[serde(rename = \"@{}\")]\n\tpub {}: {typ},", attr.name, attr.name.to_case(Case::Snake));
+                attrs.push(str);
             }
+
+            while let Some(typ) = schema.types.get(extension.base.strip_prefix("mx:").unwrap()) {
+                let name = typ.name.strip_prefix("I").unwrap();
+                let str = format!("#[serde(flatten)]\n\tpub {}: {},", name.to_case(Case::Snake), name);
+                attrs.push(str);
+
+                let Some(content) = &typ.complex_content else {
+                    break;
+                };
+                let Some(content) = &content.content else {
+                    break;
+                };
+                extension = match content {
+                    ComplexContentEl::Extension(ext) => ext.clone(),
+                    _ => break,
+                };
+            }
+
             let attrs = attrs.join("\n\t");
-            let mut res = format!("pub struct {} {{\n\t", el.name.as_ref().unwrap());
+            let mut res = format!("#[derive(Deserialize)]\npub struct {} {{\n\t", el.name.as_ref().unwrap());
             res.push_str(attrs.as_str());
             res.push_str("\n}\n");
             Some(res)
