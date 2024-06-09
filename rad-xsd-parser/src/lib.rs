0cad9aaecbdd8ceb3400592793bac059fcd08813
Brent Gardner <bgardner@squarelabs.net>
2024-06-09 11:33:29 -0600

    switch to quick-xml for rust gen

--- rad-xsd-parser/src/lib.rs
+++ rad-xsd-parser/src/lib.rs
@@ -9,13 +9,14 @@ mod tests {
     use crate::logical::LogicalSchema;
     use crate::models::Schema;
     use std::fs;
+    use quick_xml::de::from_str;
 
     #[test]
     fn it_works() {
         let filename = "../radui/resources/mxml.xsd";
         let content = fs::read_to_string(filename).expect("Unable to read file");
 
-        let schema: Schema = serde_xml_rs::from_str(&*content).unwrap();
+        let schema: Schema = from_str(&*content).unwrap();
         let logical: LogicalSchema = schema.into();
         let code = generate(logical);
         println!("{}", code);
